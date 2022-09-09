1488bfd6cbc79dfef3332a2165cb7993f473279d
hannudaya <hannudaya@gmail.com>
2022-09-09 14:10:19 -0700

    Changed the lst digit file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,9 +22,9 @@ int main(void)
 	}
 	else if (a == 0)
 	{
-		printf("Last digit of %d is %d and is zero\n", n, a);
+		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
-	else if (a > 6 && a != 0)
+	else if (a < 6 && a != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
