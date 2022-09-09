7e90c5de7ce356bf3044f7c0a9121c9e4feffca6
hannudaya <hannudaya@gmail.com>
2022-09-09 03:27:08 -0700

    Added sizes file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *  main-Entry point
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int a;
+
+	long int b;
+
+	long long c;
+
+	float d;
+
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+
+	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(c));
+
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(d));
+}
