d196330d7763b7ca26300d6dbc15abe7ee9cfc40
hannudaya <hannudaya@gmail.com>
2022-09-09 03:50:16 -0700

    Changed sizes file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,6 @@
  */
 
 int main(void)
-
 {
 	char e;
 
@@ -21,7 +20,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(e));
 
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	
+
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 
 	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(c));
