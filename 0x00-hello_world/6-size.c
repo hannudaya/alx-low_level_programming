ea89de40f8060938225b8f62204ca057240feec1
hannudaya <hannudaya@gmail.com>
2022-09-09 11:09:02 -0700

    Changed sizes file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -23,7 +23,8 @@ int main(void)
 
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(c));
 
-	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
 }
