696f6fb1ec797dfa1c4bb43dc243610c26ef5b37
hannudaya <hannudaya@gmail.com>
2022-09-09 22:18:28 -0700

    Added print alphabet to upper file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main-Entry point
+ * It prints lowercase letters a to z
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar (ch);
+		putchar (toupper(ch));
+		putchar('\n');
+	}
+	return (0);
+}
