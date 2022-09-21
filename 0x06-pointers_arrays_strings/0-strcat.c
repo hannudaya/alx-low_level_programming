03018d45b2ba00e0bd74b4f032766ff6b5f1a211
hannudaya <hannudaya@gmail.com>
2022-09-21 07:12:21 -0700

    Changed strcat file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,12 +2,17 @@
 
 /**
  * _strtcat - concatenates two stings and store in the first string
- * dest: the destination string
+ * @dest: the destination string
  * @src: the source string
  * Return: The destination string
  */
-char *_strtcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
