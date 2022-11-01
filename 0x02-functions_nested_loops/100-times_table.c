0ccd43c419e47b24a08416bed1f8efec3b4e98bd
hannudaya <hannudaya@gmail.com>
2022-11-01 03:55:37 -0700

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,7 +15,7 @@ void print_times_table(int n)
 			for (mult  = 1; mult <= n; ++mult)
 			{
 				_putchar(',');
-				_putchar(' ');
+				_putchar('\t');
 				prod = num * mult;
 				if (prod <= 9)
 					_putchar(' ');
