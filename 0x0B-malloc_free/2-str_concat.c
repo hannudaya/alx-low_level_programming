11299fac5b955c4df8edf7fdb139ffddc7a54d0e
hannudaya <hannudaya@gmail.com>
2023-08-08 10:33:20 -0700

    str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ *_strlen - count array to find the lenght of the string
+ *@s: string array of elements
+ *Return: 1
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * str_concat -concatenate two strings
+ * @s1: Array number 1
+ * @s2: Array number 2
+ * Return: An array dynamic
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *dst;
+	unsigned int i, j, size;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	size = (_strlen(s1) + _strlen(s2) + 1);
+	dst = (char *) malloc(size * sizeof(char));
+	if (dst == 0)
+		return (NULL);
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		*(dst + i) = *(s1 + i);
+	for (j = 0; *(s2 + j) != '\0'; j++)
+	{
+		*(dst + i) = *(s2 + j);
+		i++;
+	}
+	return (dst);
+}
