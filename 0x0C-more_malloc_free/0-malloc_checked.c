3de42c49b0ea1cc169b1173ffbaed2e69651cdf0
hannudaya <hannudaya@gmail.com>
2023-08-13 14:35:06 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the size of the string
+ * Return: a pointer, or Null
+ */
+void *malloc_checked(unsigned int b);
+{
+	void *nb;
+
+	nb = malloc(b);
+	if (nb == NULL)
+		exit(98);
+	return (nb);
+}
